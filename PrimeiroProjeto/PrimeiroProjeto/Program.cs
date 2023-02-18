da4abbe8787397716492587b87ecdffc7aebe255
wesley gomes <wesleypgomes2020@gmail.com>
2023-02-18 12:43:54 -0300

    primeiro programa

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -10,6 +10,13 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
+            // Criar um Programa que calcule a idade do cliente a partir da sua data de nascimento
+            var anoAtual = DateTime.Now.Year; 
+            Console.Write("Em que ano você nasceu? ");
+            int ano = int.Parse(Console.ReadLine());
+            int idade = anoAtual - ano;
+            Console.WriteLine("você tem {0} anos!", idade);
+            Console.ReadLine();
         }
     }
 }
